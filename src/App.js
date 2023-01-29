c402556e32a28a727e770673cef2617df3cffe0a
Owahed Hossain <owahedhossain@gmail.com>
2023-01-29 20:27:40 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import "./_app.scss";
 
-import { useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import LoginScreen from "./screen/loginScreen/LoginScreen";
+import HomeScreen from "./screen/homeScreen/HomeScreen";
+import Search from "./search/Search";
 
 function App() {
   const { accessToken, loading } = useSelector((state) => state.auth);
 
-  // const history = useNavigate();
-  console.log(accessToken);
+  const history = useNavigate();
+
   useEffect(() => {
-    // history("/auth");
-    if (accessToken) {
-      return <LoginScreen />;
+    if (!loading && !accessToken) {
+      history("/login");
     }
-  }, [accessToken, loading]);
+  }, [accessToken, loading, history]);
 
   return (
     <>
-      <Layout />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              {" "}
+              <HomeScreen />
+            </Layout>
+          }
+        />
+        <Route path="login" element={<LoginScreen />}></Route>
+        <Route
+          path="search"
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        ></Route>
+      </Routes>
     </>
   );
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import { RouterProvider } from "react-router-dom";
 import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
-import routes from "../router/router";
 
-const MainLayout = ({ children }) => {
+const Layout = ({ children }) => {
   const [sidebar, setSidebar] = useState(false);
   const handelSidebarToggle = () => setSidebar(!sidebar);
   return (
@@ -18,14 +16,6 @@ const MainLayout = ({ children }) => {
         </Container>
       </div>
     </>
-  );
-};
-
-const Layout = () => {
-  return (
-    <MainLayout>
-      <RouterProvider router={routes} />
-    </MainLayout>
   );
 };
 
